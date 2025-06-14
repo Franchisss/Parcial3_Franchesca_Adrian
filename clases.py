976342e84923ec82ddefbd7e8c24ca04a62ef9a9
Franchisss <franchesca.cordoba@udea.edu.co>
2025-06-14 09:41:39 -0500

    Clase 1 - Paciente

--- clases.py
+++ clases.py
@@ -5,3 +5,13 @@ import pydicom
 import matplotlib.pyplot as plt
 import nibabel as nib
 from nilearn import plotting
+
+class Paciente:
+    def __init__(self, nombre, edad, id, imagen):
+        self.nombre = nombre
+        self.edad = edad
+        self.paciente_id = id
+        self.imagen_3d = imagen
+
+
+
