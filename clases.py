a8d024aa252b97c73bca033744bff642f0d58b7d
Franchisss <franchesca.cordoba@udea.edu.co>
2025-06-14 10:36:01 -0500

    clase 2- funcion 1 - cargarDicoms

--- clases.py
+++ clases.py
@@ -14,10 +14,28 @@ class Paciente:
         self.imagen_3d = imagen
 
 class ArchivosDicom:
-    def __init__(self, carpeta):
+    def __init__(self, carpeta = "DICOMS"):
         self.carpeta = carpeta
         self.dicoms = []
         self.volumen = None
         self.dataset_ejemplo = None
+
+def cargar_dicoms(self):
+    archivos = sorted(
+        [f for f in os.listdir(self.carpeta) if f.endswith(".dcm")],
+        key=lambda x: int(x.split('.')[0])
+    )
+
+    if not archivos:
+        print("No se encontraron archivos .dcm en la carpeta.")
+        return
+
+    for archivo in archivos:
+        ruta_completa = os.path.join(self.carpeta, archivo)
+        ds = pydicom.dcmread(ruta_completa)
+        self.dicoms.append(ds)
+
+    print(f"Se cargaron {len(self.dicoms)} archivos DICOM.")
+
 
 
